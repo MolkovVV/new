6b0a81b18588af8fafaef875cce09b24d5f3dfa7
Vladislav <molkov.udmnn@gmail.com>
2023-02-20 13:47:37 +0300

    fix: delete all comments in PracticeFormTest.java fix: add tags @BeforeAll and @BeforeEach fix: replace assertion.shouldNotBe(empty) on .shouldHave(text("Male")); fix: replace construction $(".modal-dialog").isDisplayed() on $(".modal-dialog").shouldBe(visible) fix: rename test method on successfulNewStudentRegistration fix: replace locators for drop-down-list elements feature: add assertion with regexp on data string

--- src/test/java/com/demoqa/practiceform/PracticeFormTest.java
+++ src/test/java/com/demoqa/practiceform/PracticeFormTest.java
@@ -1,6 +1,8 @@
 package com.demoqa.practiceform;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -9,65 +11,52 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
-    @Test
-    @Tag("Smoke")
-    public void formIsVisible(){
+
+    @BeforeAll
+    public static void setBaseUrl() {
         Configuration.baseUrl = "https://demoqa.com/";
-        //open form
-        open("automation-practice-form");
-        //assert that form is visible (question: Can I use isDisplayed() method in this step?)
-        $("form[novalidate]#userForm").shouldBe(visible);
     }
 
     @Test
-    @Tag("Functional")
-    public void successRegistryNewStudent() {
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.browserSize = "1920x1080";
-        //open form
+    @Tag("Smoke")
+    public void formIsVisible() {
         open("automation-practice-form");
-        //add Name in input
-        $("#firstName").setValue("Jonny");
-        //add LastName in input
-        $("#lastName").setValue("Walker");
-        //add email in input
-        $("#userEmail").setValue("test@gmail.com");
-        //radio-button check
-        $("#genterWrapper").$$(".custom-control-inline").first().click();
-        //set phone number
-        $("#userNumber").setValue("9102222222");
+        $("form[novalidate]#userForm").shouldBe(visible);
+    }
 
-        //date setup (question: How insert random date with sendKeys??? it doesn`t replace default value)
-        //$("#dateOfBirthInput").sendKeys("02/20/2002");
+
+    @BeforeEach
+    public void addBrowserSize() {
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    @Tag("Functional")
+    public void successfulNewStudentRegistration() {
+        open("automation-practice-form");
+        $("#firstName").setValue("Jonny");
+        $("#lastName").setValue("Walker");
+        $("#userEmail").setValue("test@gmail.com");
+        $("#genterWrapper").$$(".custom-control-inline").first().click();
+        $("#userNumber").setValue("9102222222");
         $("#dateOfBirthInput").click();
         $$(".react-datepicker__week").first().ancestor(".").click();
-        //add text in subjectInput
         $("#subjectsInput").setValue("text");
-        //set hobbie checkbox
         $("#hobbies-checkbox-3").sendKeys(" ");
-        //set currentAddress
         $("#currentAddress").setValue("example text");
-        //set State
         $("#state").scrollTo().click();
-        $$(".css-yt9ioa-option").first().click();
-        //set City
+        $("#react-select-3-option-0").click();
         $("#city").click();
-        $$(".css-1n7v3ny-option").first().click();
-        //submit on button with bug
-        /*$("#city").click();
-        $("#submit").click();
-        sleep(4000);
-        */
-        //submit on Enter
+        $("#react-select-4-option-0").click();
         $("#city").click();
         $("#submit").pressEnter();
 
         //Asserts
-        $(".modal-dialog").isDisplayed();
+        $(".modal-dialog").shouldBe(visible);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $("tbody").find("tr",0).find("td",1).shouldHave(text("Jonny"));
-        $("tbody").find("tr",1).find("td",1).shouldHave(text("test@gmail.com"));
-        $("tbody").find("tr",2).find("td",1).shouldNotBe(empty);
-        $("tbody").find("tr",3).find("td",1).shouldHave(text("9102222222"));
+        $("tbody").find("tr", 0).find("td", 1).shouldHave(text("Jonny"));
+        $("tbody").find("tr", 1).find("td", 1).shouldHave(text("test@gmail.com"));
+        $("tbody").find("tr", 2).find("td", 1).shouldHave(text("Male"));
+        $("tbody").find("tr", 3).find("td", 1).shouldHave(text("9102222222"));
+        $("tbody").find("tr", 4).find("td", 1).shouldBe(matchText("([0-9]{2}\\s\\w{3,8}.[0-9]{4})"));
     }
 }
