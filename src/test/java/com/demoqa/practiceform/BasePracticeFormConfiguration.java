e610b19c8865cacfa941b5fcdba865f909515519
Vladislav <molkov.udmnn@gmail.com>
2023-03-28 22:53:07 +0300

    add allure report

--- src/test/java/com/demoqa/practiceform/BasePracticeFormConfiguration.java
+++ src/test/java/com/demoqa/practiceform/BasePracticeFormConfiguration.java
@@ -2,11 +2,19 @@ package com.demoqa.practiceform;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BasePracticeFormConfiguration {
     @BeforeEach
     public void addBrowserSize() {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 }
--- src/test/java/com/demoqa/practiceform/PracticeFormPojoTest.java
+++ src/test/java/com/demoqa/practiceform/PracticeFormPojoTest.java
@@ -1,8 +1,13 @@
 package com.demoqa.practiceform;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.practiceform.helpers.Attach;
 import com.demoqa.practiceform.testdata.TestStudentInit;
 import com.demoqa.practiceform.pages.StudentRegistrationForm;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class PracticeFormPojoTest extends BasePracticeFormConfiguration {
@@ -10,37 +15,49 @@ public class PracticeFormPojoTest extends BasePracticeFormConfiguration {
     StudentRegistrationForm registrationForm = new StudentRegistrationForm();
 
     @Test
-    public void successAllElementsRegistration(){
+    public void successAllElementsRegistration() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         TestStudentInit localTestStudentsData = new TestStudentInit();
+        step("Открываем форму регистрации нового студента", () -> {
+            registrationForm.openForm()
+                    .removeBanner()
+                    .removeFooter();
+        });
 
-        registrationForm.openForm()
-                        .removeBanner()
-                        .removeFooter()
-                        .setFirstName(localTestStudentsData.getTestFirstName())
-                        .setLastName(localTestStudentsData.getTestLastName())
-                        .setEmail(localTestStudentsData.getTestEmail())
-                        .setGender(localTestStudentsData.getGenderTest())
-                        .setPhoneNumber(localTestStudentsData.getTestMobilePhone())
-                        .setDate(localTestStudentsData.getTestDay(), localTestStudentsData.getTestMonth(), localTestStudentsData.getTestYear())
-                        .setSubjectsValue(localTestStudentsData.getTestSubject())
-                        .setHobbiesValue(localTestStudentsData.getTestHobby())
-                        .uploadFileFromClassPath(localTestStudentsData.getTestFileClassPath())
-                        .setCurrentAddress(localTestStudentsData.getTestCurrentAddress())
-                        .setState(localTestStudentsData.getTestState())
-                        .setCity(localTestStudentsData.getTestCity())
-                        .submitForm();
+        step("Заполняем форму тестовыми данными", () -> {
+            registrationForm.setFirstName(localTestStudentsData.getTestFirstName())
+                    .setLastName(localTestStudentsData.getTestLastName())
+                    .setEmail(localTestStudentsData.getTestEmail())
+                    .setGender(localTestStudentsData.getGenderTest())
+                    .setPhoneNumber(localTestStudentsData.getTestMobilePhone())
+                    .setDate(localTestStudentsData.getTestDay(), localTestStudentsData.getTestMonth(), localTestStudentsData.getTestYear())
+                    .setSubjectsValue(localTestStudentsData.getTestSubject())
+                    .setHobbiesValue(localTestStudentsData.getTestHobby())
+                    .uploadFileFromClassPath(localTestStudentsData.getTestFileClassPath())
+                    .setCurrentAddress(localTestStudentsData.getTestCurrentAddress())
+                    .setState(localTestStudentsData.getTestState())
+                    .setCity(localTestStudentsData.getTestCity());
+        });
 
-        registrationForm.verifyModalAppears()
-                        .verifyTableInModalWindow("Label","Values")
-                        .verifyTableInModalWindow("Student Name", localTestStudentsData.getTestFirstName() + " " + localTestStudentsData.getTestLastName())
-                        .verifyTableInModalWindow("Student Email",localTestStudentsData.getTestEmail())
-                        .verifyTableInModalWindow("Gender", localTestStudentsData.getGenderTest())
-                        .verifyTableInModalWindow("Mobile", localTestStudentsData.getTestMobilePhone())
-                        .verifyTableInModalWindow("Date of Birth", localTestStudentsData.getTestDay() + " " + localTestStudentsData.getTestMonth() + "," + localTestStudentsData.getTestYear())
-                        .verifyTableInModalWindow("Subjects", localTestStudentsData.getTestSubject())
-                        .verifyTableInModalWindow("Hobbies", localTestStudentsData.getTestHobby())
-                        .verifyTableInModalWindow("Picture", localTestStudentsData.getTestFileName())
-                        .verifyTableInModalWindow("Address", localTestStudentsData.getTestCurrentAddress())
-                        .verifyTableInModalWindow("State and City", localTestStudentsData.getTestState() + " " + localTestStudentsData.getTestCity());
+        step("Отправить", () -> {
+            registrationForm.submitForm();
+        });
+
+        step("Провалидировать данные полльзователя в pop-up окне успешной регистрации", () -> {
+            registrationForm.verifyModalAppears()
+                    .verifyTableInModalWindow("Label", "Values")
+                    .verifyTableInModalWindow("Student Name", localTestStudentsData.getTestFirstName() + " " + localTestStudentsData.getTestLastName())
+                    .verifyTableInModalWindow("Student Email", localTestStudentsData.getTestEmail())
+                    .verifyTableInModalWindow("Gender", localTestStudentsData.getGenderTest())
+                    .verifyTableInModalWindow("Mobile", localTestStudentsData.getTestMobilePhone())
+                    .verifyTableInModalWindow("Date of Birth", localTestStudentsData.getTestDay() + " " + localTestStudentsData.getTestMonth() + "," + localTestStudentsData.getTestYear())
+                    .verifyTableInModalWindow("Subjects", localTestStudentsData.getTestSubject())
+                    .verifyTableInModalWindow("Hobbies", localTestStudentsData.getTestHobby())
+                    .verifyTableInModalWindow("Picture", localTestStudentsData.getTestFileName())
+                    .verifyTableInModalWindow("Address", localTestStudentsData.getTestCurrentAddress())
+                    .verifyTableInModalWindow("State and City", localTestStudentsData.getTestState() + " " + localTestStudentsData.getTestCity());
+
+        });
+        Attach.addVideo();
     }
 }
