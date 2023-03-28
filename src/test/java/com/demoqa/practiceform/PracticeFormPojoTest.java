257135d9d25028fd0b5ceca2922248ab4d62b33d
Vladislav <molkov.udmnn@gmail.com>
2023-03-29 00:11:03 +0300

    add another steps description

--- src/test/java/com/demoqa/practiceform/PracticeFormPojoTest.java
+++ src/test/java/com/demoqa/practiceform/PracticeFormPojoTest.java
@@ -8,9 +8,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.Allure.step;
-
-
 public class PracticeFormPojoTest extends BasePracticeFormConfiguration {
 
     StudentRegistrationForm registrationForm = new StudentRegistrationForm();
@@ -20,32 +17,23 @@ public class PracticeFormPojoTest extends BasePracticeFormConfiguration {
         SelenideLogger.addListener("allure", new AllureSelenide());
         TestStudentInit localTestStudentsData = new TestStudentInit();
 
-        step("Открываем форму регистрации нового студента", () -> {
             registrationForm.openForm()
                             .removeBanner()
-                            .removeFooter();
-        });
+                            .removeFooter()
+                            .setFirstName(localTestStudentsData.getTestFirstName())
+                            .setLastName(localTestStudentsData.getTestLastName())
+                            .setEmail(localTestStudentsData.getTestEmail())
+                            .setGender(localTestStudentsData.getGenderTest())
+                            .setPhoneNumber(localTestStudentsData.getTestMobilePhone())
+                            .setDate(localTestStudentsData.getTestDay(), localTestStudentsData.getTestMonth(), localTestStudentsData.getTestYear())
+                            .setSubjectsValue(localTestStudentsData.getTestSubject())
+                            .setHobbiesValue(localTestStudentsData.getTestHobby())
+                            .uploadFileFromClassPath(localTestStudentsData.getTestFileClassPath())
+                            .setCurrentAddress(localTestStudentsData.getTestCurrentAddress())
+                            .setState(localTestStudentsData.getTestState())
+                            .setCity(localTestStudentsData.getTestCity())
+                            .submitForm();
 
-        step("Заполняем форму тестовыми данными", () -> {
-            registrationForm.setFirstName(localTestStudentsData.getTestFirstName())
-                    .setLastName(localTestStudentsData.getTestLastName())
-                    .setEmail(localTestStudentsData.getTestEmail())
-                    .setGender(localTestStudentsData.getGenderTest())
-                    .setPhoneNumber(localTestStudentsData.getTestMobilePhone())
-                    .setDate(localTestStudentsData.getTestDay(), localTestStudentsData.getTestMonth(), localTestStudentsData.getTestYear())
-                    .setSubjectsValue(localTestStudentsData.getTestSubject())
-                    .setHobbiesValue(localTestStudentsData.getTestHobby())
-                    .uploadFileFromClassPath(localTestStudentsData.getTestFileClassPath())
-                    .setCurrentAddress(localTestStudentsData.getTestCurrentAddress())
-                    .setState(localTestStudentsData.getTestState())
-                    .setCity(localTestStudentsData.getTestCity());
-        });
-
-        step("Отправить", () -> {
-            registrationForm.submitForm();
-        });
-
-        step("Провалидировать данные полльзователя в pop-up окне успешной регистрации", () -> {
             registrationForm.verifyModalAppears()
                     .verifyTableInModalWindow("Label", "Values")
                     .verifyTableInModalWindow("Student Name", localTestStudentsData.getTestFirstName() + " " + localTestStudentsData.getTestLastName())
@@ -59,7 +47,6 @@ public class PracticeFormPojoTest extends BasePracticeFormConfiguration {
                     .verifyTableInModalWindow("Address", localTestStudentsData.getTestCurrentAddress())
                     .verifyTableInModalWindow("State and City", localTestStudentsData.getTestState() + " " + localTestStudentsData.getTestCity());
 
-        });
-        Attach.addVideo();
+            Attach.addVideo();
     }
 }
--- src/test/java/com/demoqa/practiceform/pages/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/practiceform/pages/StudentRegistrationForm.java
@@ -3,6 +3,7 @@ package com.demoqa.practiceform.pages;
 import com.codeborne.selenide.SelenideElement;
 import com.demoqa.practiceform.components.DatePicker;
 import com.demoqa.practiceform.components.SuccessModalWindow;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -27,47 +28,56 @@ public class StudentRegistrationForm {
     private final SelenideElement CITY_STATE_ELEMENT_WRAPPER = $("#stateCity-wrapper");
     private final SelenideElement SUBMIT_BUTTON =  $("#submit");
 
+    @Step("Открываем форму регистрации")
     public StudentRegistrationForm openForm(){
         open("automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         return this;
     }
 
+    @Step("Убираем banner")
     public StudentRegistrationForm removeBanner(){
         executeJavaScript("$('#fixedban').remove()");
         return this;
     }
 
+    @Step("Убираем footer")
     public StudentRegistrationForm removeFooter(){
         executeJavaScript("$('footer').remove()");
         return this;
     }
 
+    @Step("В input FirstName вводим значение {firstName}")
     public StudentRegistrationForm setFirstName(String firstName){
         FIRST_NAME_INPUT.setValue(firstName);
         return this;
     }
 
+    @Step("В input LastName вводим значение {lastName}")
     public StudentRegistrationForm setLastName(String lastName){
         LAST_NAME_INPUT.setValue(lastName);
         return this;
     }
 
+    @Step("В input Email вводим значение {email}")
     public StudentRegistrationForm setEmail(String email){
         EMAIL_INPUT.setValue(email);
         return this;
     }
 
+    @Step("Устанавливаем значение {gender} для поля Gender")
     public StudentRegistrationForm setGender(String gender){
         GENDER_BUTTONS_ROW.$(byText(gender)).click();
         return this;
     }
 
+    @Step("В поле PhoneNumber вводим значение {phoneNumber}")
     public StudentRegistrationForm setPhoneNumber(String phoneNumber){
         MOBILE_NUMBER_INPUT.setValue(phoneNumber);
         return this;
     }
 
+    @Step("В поле BirthdayDate устанавливаем значение {day}/{monthName}/{year}")
     public StudentRegistrationForm setDate(String day, String monthName, String year){
         DATE_OF_BIRTH_INPUT.click();
         datePickerOnStudentRegistrationForm.checkVisibilityDatePicker();
@@ -78,47 +88,56 @@ public class StudentRegistrationForm {
         return this;
     }
 
+    @Step("В поле Subject устанавливаем значение {subjects}")
     public StudentRegistrationForm setSubjectsValue(String subjects){
         SUBJECTS_INPUT.setValue(subjects).pressEnter();
         return this;
     }
 
+    @Step("В поле Hobby устанавливаем значение {hobby}")
     public StudentRegistrationForm setHobbiesValue(String hobby){
         HOBBIES_ROW.$(byText(hobby)).click();
         return this;
     }
 
+    @Step("В поле UploadFile загружаем файл {filename}")
     public StudentRegistrationForm uploadFileFromClassPath(String filename){
         UPLOAD_IMG_INPUT.uploadFromClasspath(filename);
         return this;
     }
 
+    @Step("В поле currentAddress устанавливаем значение {currentAddress}")
     public StudentRegistrationForm setCurrentAddress(String currentAddress){
         ADDRESS_TEXT_AREA.setValue(currentAddress);
         return this;
     }
 
+    @Step("В поле City устанавливаем значение {city}")
     public StudentRegistrationForm setCity(String city){
         CITY_DROPDOWN.click();
         CITY_STATE_ELEMENT_WRAPPER.$(byText(city)).click();
         return this;
     }
 
+    @Step("В поле State устанавливаем значение {state}")
     public StudentRegistrationForm setState(String state){
         STATE_DROPDOWN.click();
         CITY_STATE_ELEMENT_WRAPPER.$(byText(state)).click();
         return this;
     }
 
+    @Step("Отправили форму")
     public void submitForm(){
         SUBMIT_BUTTON.click();
     }
 
+    @Step("Проверить отображение pop-up окна при успешной регистрации")
     public StudentRegistrationForm verifyModalAppears(){
         modalWindow.registrationModalVerify();
         return this;
     }
 
+    @Step("В pop-up окне значение для {key} соответствует {value}")
     public StudentRegistrationForm verifyTableInModalWindow(String key, String value){
         modalWindow.verifyResultInTable(key,value);
         return this;
