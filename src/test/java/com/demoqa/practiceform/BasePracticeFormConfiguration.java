53d71c96d24af00f4de453efb9d0e5d3a5739124
Vladislav <molkov.udmnn@gmail.com>
2023-04-01 13:58:21 +0300

    add properties usage

--- src/test/java/com/demoqa/practiceform/BasePracticeFormConfiguration.java
+++ src/test/java/com/demoqa/practiceform/BasePracticeFormConfiguration.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class BasePracticeFormConfiguration {
     @BeforeEach
     public void addBrowserSize() {
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com/");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidAddress","selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
